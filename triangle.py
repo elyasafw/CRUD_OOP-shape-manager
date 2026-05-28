157ffbae3de951e7173ff857bf9a184ce5b18910
elyasaf waisman <elyasafw1997@gmail.com>
2026-05-28 23:14:01 +0300

    create triangle class

---
+++ triangle.py
@@ -0,0 +1,28 @@
+from shape import Shape
+from math import sqrt
+
+
+class Triangle(Shape):
+    def __init__(self, shape_id, shape_type, a, b, c):
+        super().__init__(shape_id, shape_type)
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def get_area(self):
+        s = self.get_perimeter() / 2
+        return sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
+
+    def get_perimeter(self):
+        return self.a + self.b + self.c
+
+    def to_dict(self):
+        return {
+            "ID": self.id,
+            "Type": self.type,
+            "Side A": self.a,
+            "Side B": self.b,
+            "Side C": self.c,
+            "Area": self.get_area(),
+            "Perimeter": self.get_perimeter()
+        }
