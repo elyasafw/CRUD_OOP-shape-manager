6b5d7de32622744a624dfc7011e682050a07fa6b
elyasaf waisman <elyasafw1997@gmail.com>
2026-06-03 21:11:03 +0300

    added POST "create shape" function

--- server.py
+++ server.py
@@ -46,3 +46,26 @@ def get_shape(id: int):
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Shape ID not found in shapes list.."
         )
+
+
+@app.post('/shapes')
+def create_shape(new_shape: ShapeModel):
+    next_id = len(MANAGER.shapes) + 1
+    clean_shape_dict = new_shape.model_dump(exclude_unset=True)
+    clean_shape_dict["ID"] = next_id
+    for shape in MANAGER.shapes:
+        if shape.id == next_id:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Shape id already in shapes list !"
+                )
+    new_shape_object = MANAGER._dict_to_shape(clean_shape_dict)
+    if not new_shape_object:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid shape type")
+    MANAGER.create_shape(new_shape_object)
+    return {
+        "message": "Shape added successfully",
+        "data": new_shape_object.to_dict(),
+    }
